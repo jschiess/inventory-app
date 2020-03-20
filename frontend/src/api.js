7abe8e806b20e9f0c5bf547caba8013766bf4648
holoarcher <josiah.schiess@gmail.com>
2020-03-20 13:35:41 +0100

    fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import store from './store'
 
 export default () => {
 	return axios.create({
-		baseURL: `${window.location.origin}:${process.env.VUE_APP_API_PORT || 1337}/api/`,
+		baseURL: `http://${window.location.hostname}:${process.env.VUE_APP_API_PORT || 1337}/api/`,
 		headers: {
 			"Access-Control-Allow-Origin": "*",
 			"Authorization": "Bearer " + store.state.token
