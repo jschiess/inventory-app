3ddff5e8ea516ff79bb3dc7dbb54beb9641b1e63
holoarcher <josiah.schiess@gmail.com>
2020-03-20 02:03:59 +0100

    .

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -33,22 +33,14 @@ export default {
 	methods: {
 		login: async function() {
 			if (this.$refs.form.validate()) {
-				
 				try {
-					
 					this.$store.dispatch('login', this.form).then(() => {
-						
 						this.$router.push("/");
 					})
-					
 					this.$emit("message", { type: "success", text: 'erfolgreich eingeloggt', timeout: 1000 });
-
 				} catch (error) {
 					this.$emit("message", { type: "error", text: error.message, timeout: 0 });
-				
 				}
-
-
 			}
 		}
 	}
