bbc02324c87966286352f9197937dab60187463d
holoarcher <josiah.schiess@gmail.com>
2020-03-20 02:01:11 +0100

    fixed url error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import store from './store'
 
 export default () => {
 	return axios.create({
-		baseURL: `http://localhost:${process.env.VUE_APP_API_PORT || 1337}/api/`,
+		baseURL: `${window.location.origin}:${process.env.VUE_APP_API_PORT || 1337}/api/`,
 		headers: {
 			"Access-Control-Allow-Origin": "*",
 			"Authorization": "Bearer " + store.state.token
